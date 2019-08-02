c36f4bad4265bcae2b44d6a92922552867cb1f55
CHEBBAH Mehdi <gm_chebbah@esi.dz>
2019-08-02 18:15:08 +0100

    third try

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var http = require('http');
+const PORT = process.env.PORT || 8080;
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write(req.url);
   res.end();
-}).listen(8080);
+}).listen(PORT);
